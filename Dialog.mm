006368a2156ef5fdc68c15288b95b5ab71dd2738
Ronald Wampler <rdwampler@gmail.com>
2016-07-16 13:59:01 -0400

    Reinstate "Replace deprecated method for initializing nibs (10.8)"
    This reinstates commit 6ce0d06 ("Replace deprecated method for
    initializing nibs (10.8)"). The problem was the use of NSWorkspace's
    `isFilePackageAtPath:` to determine if the nib was compiled or not,
    since it appears that Xcode.app defines .nib as a file package and not
    the OS. Therefore, the previous check would fail for users without
    Xcode installed.
    
    This version just simply checks if the file keyedobjects.nib exist.

--- Dialog.mm
+++ Dialog.mm
@@ -509,16 +509,19 @@ static NSUInteger sNextWindowControllerToken = 1;
 	if(initialValues && [initialValues count])
 		[[NSUserDefaults standardUserDefaults] registerDefaults:initialValues];
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-	NSNib* nib = [[NSNib alloc] initWithContentsOfURL:[NSURL fileURLWithPath:aNibPath]];
+	NSData* nibData;
+	NSString* keyedObjectsNibPath = [aNibPath stringByAppendingPathComponent:@"keyedobjects.nib"];
+	if([[NSFileManager defaultManager] fileExistsAtPath:keyedObjectsNibPath])
+		nibData = [NSData dataWithContentsOfFile:keyedObjectsNibPath];
+	else	nibData = [NSData dataWithContentsOfFile:aNibPath];
+
+	NSNib* nib = [[NSNib alloc] initWithNibData:nibData bundle:nil];
 
 	if(!nib)
 	{
 		NSLog(@"%s failed loading nib: %@", sel_getName(_cmd), aNibPath);
 		return nil;
 	}
-#pragma clang diagnostic pop
 
 	TMDNibWindowController* nibOwner = [[TMDNibWindowController alloc] initWithParameters:someParameters modal:modal center:shouldCenter aysnc:async];
 	if(!nibOwner)
